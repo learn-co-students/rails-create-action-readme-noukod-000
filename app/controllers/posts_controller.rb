e96bc5ec3bf772908ec5c7384751e6f6c0a2054d
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 16:50:55 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,20 @@
 class PostsController < ApplicationController
-	def index
-		@posts = Post.all
-	end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def index
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+  end
+
+  def show
+    @post=Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
 end
